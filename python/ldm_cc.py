5c4745cc984e82fccc2fbe04bb313ac5731b4824
ViriMares <virimaresoui@gmail.com>
2018-05-20 11:22:40 -0500

    LDM terminado

--- python/ldm_cc.py
+++ python/ldm_cc.py
@@ -26,17 +26,27 @@ class ldm_cc(gr.hier_block2):
     """
     docstring for block ldm_cc
     """
-    def __init__(self, multiple):
+    def __init__(self, multiplier):
         gr.hier_block2.__init__(self,
             "ldm_cc",
             gr.io_signature(2, 2, gr.sizeof_gr_complex),  # Input signature
-            gr.io_signature(1, 1, gr.sizeof_gr_complex)) # Output signature
+            gr.io_signature(1, 1, gr.sizeof_gr_complex))  # Output signature
 
-    	self.blocks_multiply_const_vxx = blocks.multiply_const_vcc((multiple, ))
-    	self.blocks_add_xx= blocks.add_vcc(1)
-        
+        normalizing = { 1.0000000: 0.7071068, 0.9440609: 0.7271524, 0.8912509: 0.7465331, 0.8413951: 0.7651789, 0.7943282: 0.7830305, 0.7498942: 0.8000406, 0.7079458: 0.8161736, 0.6683439: 0.8314061, 0.6309573: 0.8457262, 0.5956621: 0.8591327, 0.5623413: 0.8716346, 0.5011872: 0.8940022, 0.4466836: 0.9130512, 0.3981072: 0.9290819, 0.3548134: 0.9424353, 0.3162278: 0.9534626, 0.2818383: 0.9625032, 0.2511886: 0.9698706, 0.2238721: 0.9758449, 0.1995262: 0.9806699, 0.1778279: 0.9845540, 0.1584893: 0.9876723, 0.1412538: 0.9901705, 0.1258925: 0.9921685, 0.1122018: 0.9937642, 0.1000000: 0.9950372, 0.0891251: 0.9960519, 0.0794328: 0.9968601, 0.0707946: 0.9975034, 0.0630957: 0.9980154, 0.0562341: 0.9984226 }
+        norm = normalizing[multiplier]
+
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((multiplier, ))
+    	self.blocks_add_xx = blocks.add_vcc(1)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vcc((norm, ))
+
     	# Define blocks and connect them
-    	self.connect((self,0), (self.blocks_add_xx,0))
-    	self.connect((self,1), self.blocks_multiply_const_vxx)
-    	self.connect(self.blocks_multiply_const_vxx, (self.blocks_add_xx, 1))
-    	self.connect(self.blocks_add_xx, self)
+    	self.connect((self, 0), (self.blocks_add_xx, 0))
+    	self.connect((self, 1), self.blocks_multiply_const_vxx_0)
+    	self.connect(self.blocks_multiply_const_vxx_0, (self.blocks_add_xx, 1))
+        self.connect(self.blocks_add_xx, self.blocks_multiply_const_vxx_1)
+    	self.connect(self.blocks_multiply_const_vxx_1, self)
+
+        print
+        print "LDM:"
+        print "Multiplier Factor Alfa =", multiplier
+        print "Normalizing Factor Beta=", norm
